7d264a21c2ce3ed75ed1a6d061570d27e4549cce
adminadmin1 <davidk@ravtech.co.il>
2023-02-20 13:42:37 +0200

    Added discovery api. Added the flow of kafka messages until the delivery server.

--- common/src/topics/topics.enusm.ts
+++ common/src/topics/topics.enusm.ts
@@ -2,7 +2,11 @@ export enum UploadTopics {
     NEW_VERSION = 'upload.new-version'
 }
 
-export enum DeliveryTopics{
+export enum DeliveryTopics {
     PREPARE_PACKAGE = "delivery.prepare-package",
     GET_PREPARED_PACKAGE = "delivery.get-prepared-package"
+}
+
+export enum OfferingTopics {
+    CHACK_UPDATES = "offering.chack-updates",
 }
