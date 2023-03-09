d1f3abb09bd90bf5b76ccf1944097f8e8d8ebe03
Ari Havlin <ayhavlin@gmail.com>
2023-03-09 14:10:54 +0200

    create a project-management micro-service and change all the api references from dashboard to project-management

--- common/src/topics/topics.enusm.ts
+++ common/src/topics/topics.enusm.ts
@@ -16,12 +16,12 @@ export enum OfferingTopics {
     CHECK_UPDATES = "offering.check-updates",
 }
 
-export enum DashboardTopics{
-    GET_USER_PROJECTS = 'dashboard.get-user-projects',
-    ADD_PROJECT = 'dashboard.add-project',
-    CREATE_TOKEN = 'dashboard.create-token',
-    ADD_NEW_MEMBER = 'dashboard.add-new-member',
-    // DELETE_MEMBER = 'dashboard.delete-member',
-    // EDIT_MEMBER = 'dashboard.edit-member'
+export enum ProjectManagementTopics{
+    GET_USER_PROJECTS = 'project-management.get-user-projects',
+    ADD_PROJECT = 'project-management.add-project',
+    CREATE_TOKEN = 'project-management.create-token',
+    ADD_NEW_MEMBER = 'project-management.add-new-member',
+    // DELETE_MEMBER = 'project-management.delete-member',
+    // EDIT_MEMBER = 'project-management.edit-member'
 
 }
