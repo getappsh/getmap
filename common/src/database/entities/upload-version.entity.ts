a4518ff9d4275b2b49b5ddb6514b922b44b705ae
Ari Havlin <ayhavlin@gmail.com>
2023-03-23 10:51:22 +0200

    Change upload-version table to be more dynamic

--- common/src/database/entities/upload-version.entity.ts
+++ common/src/database/entities/upload-version.entity.ts
@@ -1,4 +1,3 @@
-import { IsUrl } from "class-validator";
 import { Entity, Column, Unique, ManyToOne } from "typeorm";
 import { BaseEntity } from "./base.entity";
 import { ProjectEntity } from "./project.entity";
@@ -6,7 +5,6 @@ import { ProjectEntity } from "./project.entity";
 @Entity('upload_version')
 @Unique('platform_component_formation_version_unique_constraint', ['platform', 'component', 'formation', 'version'])
 export class UploadVersionEntity extends BaseEntity{
-// TODO change db columns to be only the shard columns of artifact and manifest and the rest will bee save as a json
     @Column({name: "platform"})
     platform: string;
 
@@ -22,30 +20,8 @@ export class UploadVersionEntity extends BaseEntity{
     @Column ({name: 'version'})
     version: string
 
-    @Column({name: "release_notes", nullable: true})
-    releaseNotes: string
-
-    @Column({name:'base_version', nullable: true})
-    baseVersion: string
-
-    @Column({name:'previous_version', nullable: true})
-    previousVersion: string
-
-    @Column({name:'size', nullable: true})
-    size: string
-
-    @Column({name:'agentTags', nullable: true})
-    agent_tags: string
-
-    @Column({name:'install_type', nullable: true})
-    installType: string
-
-    @Column({name:'artifact_type', nullable: true})
-    artifactType: string
-
-    @Column({name: 'url'})
-    @IsUrl()
-    url: string 
+    @Column('jsonb',{name: 'metadata', default: {}})
+    metadata: any
 
     @Column({name: 's3_url', nullable: true})
     s3Url: string
@@ -62,46 +38,36 @@ export class UploadVersionEntity extends BaseEntity{
     @ManyToOne(() => ProjectEntity)
     project: ProjectEntity
 
-    static fromArtifact(artifactDto: any){
+    static fromArtifact({platform, component, formation, OS, version, project, ...metadata}){
         const newVersion = new UploadVersionEntity()
-        newVersion.platform = artifactDto.platform;
-        newVersion.component = artifactDto.component;
-        newVersion.formation = artifactDto.formation;
-        newVersion.OS = artifactDto.OS
-        newVersion.version = artifactDto.version;
-        newVersion.releaseNotes = artifactDto.releaseNotes;
-        newVersion.size = artifactDto.size;
-        newVersion.url = artifactDto.url;
-        newVersion.artifactType = artifactDto.artifactType;
-        newVersion.project = artifactDto.project
+        newVersion.platform = platform;
+        newVersion.component = component;
+        newVersion.formation = formation;
+        newVersion.OS = OS;
+        newVersion.version = version;
+        newVersion.project = project;
+        
+        newVersion.metadata = metadata;      
 
         return newVersion
     }
 
-    static fromManifest(manifest: any){
+    static fromManifest({product, name, formation, version, project, url, ...metadata}){
         const newVersion = new UploadVersionEntity()
+        newVersion.platform = product;
+        newVersion.component = name;
+        newVersion.formation = formation;
+        newVersion.version = version;
+        newVersion.project = project;
+        newVersion.s3Url = url;
 
-        newVersion.platform = manifest.product;
-        newVersion.component = manifest.name;
-        newVersion.formation = manifest.formation;
-        newVersion.agent_tags = manifest.agentTags;
-        newVersion.version = manifest.version;
-        newVersion.previousVersion = manifest.preVersion;
-        newVersion.baseVersion = manifest.baseVersion;
-        newVersion.installType = manifest.installType;
-        newVersion.size = manifest.size;
-        newVersion.s3Url = manifest.url;
-        newVersion.project = manifest.project;
+        newVersion.metadata = metadata;
 
-        // "properties"{
-        //     "items":[{"key":"","value":""}]} 
         return newVersion;
-
     }
     
     toString(){
         return JSON.stringify(this)
     }
- 
 
 }
