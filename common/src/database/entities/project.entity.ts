1b85e5a16cd21358de5f63ee4902477d55778996
Ari Havlin <ayhavlin@gmail.com>
2023-05-18 19:02:37 +0300

    Update project and member entities

--- common/src/database/entities/project.entity.ts
+++ common/src/database/entities/project.entity.ts
@@ -1,21 +1,40 @@
-import { Column, Entity, Index, OneToMany } from "typeorm";
+import { Column, Entity, Index, ManyToOne, OneToMany } from "typeorm";
 import { BaseEntity } from "./base.entity";
 import { MemberProjectEntity } from "./member_project.entity";
+import { MemberEntity } from "./member.entity";
 
 
 @Entity("project")
 export class ProjectEntity extends BaseEntity{
     
-    @Index("project_name_unique_constraint", {unique: true})
-    @Column({name: "name"})
-    name: string
+    @Index("project_component_name_unique_constraint", {unique: true})
+    @Column({name: "component_name"})
+    componentName: string;
+
+    @Column({name: "OS", default: null})
+    OS: string;
+
+    @Column({name: "platform_type", default: null})
+    platformType: string;
+
+    @Column({name: "formation", default: null})
+    formation: string;
+
+    @Column({name: "artifact_type", default: null})
+    artifactType: string;
+
+    @Column({name: "category", default: null})
+    category: string;
 
     @Column({name: "description"})
     description: string
-
+    
     @Column('simple-array', {name: "tokens", nullable: true})
     tokens: string[]
 
+    @ManyToOne(() => MemberEntity)
+    memberOwner: MemberEntity;
+   
     @OneToMany(() => MemberProjectEntity, memberProject => memberProject)
     memberProject: MemberProjectEntity[];
 
