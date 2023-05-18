34f79c6be0a606d355ec6f4144104b844433dfb6
Ari Havlin <ayhavlin@gmail.com>
2023-05-18 19:02:37 +0300

    Update project and member entities

--- common/src/database/entities/member.entity.ts
+++ common/src/database/entities/member.entity.ts
@@ -6,10 +6,6 @@ import { MemberProjectEntity } from "./member_project.entity";
 @Entity("member")
 export class MemberEntity extends BaseEntity{
     
-    @Index({unique: true})
-    @Column({name: "member_id"})
-    member_id: string;
-
     @Column({name: "first_name"})
     firstName: string;
 
@@ -19,9 +15,6 @@ export class MemberEntity extends BaseEntity{
     @Index("member_email_unique_constraint", {unique: true})
     @Column({name: "email"})
     email: string;
-
-    @Column({name: "default_project", default: null})
-    defaultProject: string;
 
     @OneToMany(() => MemberProjectEntity, memberProject => memberProject)
     memberProjects: MemberProjectEntity[];
