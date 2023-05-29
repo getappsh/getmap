bd81efc37d5a9c7e2dc7ddd40cb99d39d0a6a4f8
Ari Havlin <ayhavlin@gmail.com>
2023-05-29 15:44:16 +0300

    Add foreign key of device to discovery message

--- common/src/database/entities/discovery-message.entity.ts
+++ common/src/database/entities/discovery-message.entity.ts
@@ -1,18 +1,22 @@
-import { Column, Entity } from "typeorm";
+import { Column, Entity, JoinColumn, ManyToOne } from "typeorm";
 import { BaseEntity } from "./base.entity";
 import { DiscoveryType, Formation, OS } from "./enums.entity";
+import { DeviceEntity } from "./device.entity";
 
 
 
 @Entity("discovery_message")
 export class DiscoveryMessageEntity extends BaseEntity{
 
+    @ManyToOne(() => DeviceEntity)
+    @JoinColumn()
+    device: DeviceEntity;
+
     @Column('jsonb', {name: "personal_device", nullable: true})
     personalDevice: any;
 
     @Column('jsonb', {name: "situational_device", nullable: true})
     situationalDevice: any;
-
 
     @Column('jsonb', {name: "discovery_data", nullable: true})
     discoveryData: any;
