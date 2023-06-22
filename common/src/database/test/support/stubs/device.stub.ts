bef1cf86bf917861e7386049a9348020a93a4e1e
Ari Havlin <ayhavlin@gmail.com>
2023-06-22 12:59:09 +0300

    Create device sub and repo mock

---
+++ common/src/database/test/support/stubs/device.stub.ts
@@ -0,0 +1,16 @@
+import { DeviceEntity } from "@app/common/database/entities";
+import { physicalDiscoveryDtoStub } from "@app/root/api/src/modules/device/stubs/dicsocvery/discovery-general.dto.stub";
+
+export const deviceEntityStub = (): DeviceEntity => {
+  const physicalDevice = physicalDiscoveryDtoStub()
+  return {
+    ID: physicalDevice.ID,
+    MAC: physicalDevice.MAC,
+    IP: physicalDevice.IP,
+    OS: physicalDevice.OS,
+    serialNumber: physicalDevice.serialNumber,
+    possibleBandwidth: physicalDevice.possibleBandwidth,
+    availableStorage: physicalDevice.availableStorage,
+    // components: []
+  } as DeviceEntity
+};
