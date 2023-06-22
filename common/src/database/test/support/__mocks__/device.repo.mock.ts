b94907a9e2563e59d7cd672e5c8d3f01b94288c0
Ari Havlin <ayhavlin@gmail.com>
2023-06-22 14:26:23 +0300

    Fix and update discovery service tests

--- common/src/database/test/support/__mocks__/device.repo.mock.ts
+++ common/src/database/test/support/__mocks__/device.repo.mock.ts
@@ -3,5 +3,7 @@ import { deviceEntityStub } from "../stubs";
 export const mockDeviceRepo = () => {
   return {
     findOne: jest.fn().mockResolvedValue(deviceEntityStub()),
+    create: jest.fn().mockResolvedValue(deviceEntityStub()),
+    save: jest.fn().mockResolvedValue(deviceEntityStub()),
   }
 };
