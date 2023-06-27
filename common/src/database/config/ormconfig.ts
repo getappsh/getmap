51e469a882f08da84ce6327434eef0e29fbdb983
Ari Havlin <ayhavlin@gmail.com>
2023-06-27 15:03:58 +0300

    Update typeorm in order to makes migration work

--- common/src/database/config/ormconfig.ts
+++ common/src/database/config/ormconfig.ts
@@ -1,9 +1,6 @@
 import 'dotenv/config';
-import * as path from 'path';
 import { DataSource } from 'typeorm';
-import { UploadVersionEntity, ProjectEntity, MemberProjectEntity, MemberEntity, VersionPackagesEntity, DiscoveryMessageEntity, DeployStatusEntity } from '../entities';
-import { Init1680597327216 } from '../migration/1680597327216-Init';
-import { DeliveryStatusEntity } from '../entities/delivery-status.entity';
+import { UploadVersionEntity, ProjectEntity, MemberProjectEntity, MemberEntity, VersionPackagesEntity, DiscoveryMessageEntity, DeployStatusEntity, DeliveryStatusEntity } from '../entities';
 
 
 const ormConfig = new DataSource({
@@ -24,10 +21,7 @@ const ormConfig = new DataSource({
     DeliveryStatusEntity,
     DeployStatusEntity
   ],
-  migrations: [
-    // path.resolve(`${__dirname}/../migration/*.ts`),
-    Init1680597327216
-  ],
+  migrations: ['libs/common/src/database/migration/*.ts'],
   logging: false,
   synchronize: true,
   migrationsTableName: "history",
