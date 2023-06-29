b18d31b6c9c557a05a6d7f9872842bd20737d969
Ari Havlin <ayhavlin@gmail.com>
2023-06-29 13:36:14 +0300

    Create unit test for uploadArtifact in upload service

--- common/src/database/test/support/__mocks__/upload-version.repo.mock.ts
+++ common/src/database/test/support/__mocks__/upload-version.repo.mock.ts
@@ -2,6 +2,7 @@ import { uploadVersionEntityStub } from "../stubs";
 
 export const mockUploadVersionRepo = () => {
   return {
+    save: jest.fn().mockResolvedValue(uploadVersionEntityStub()),
     findOne: jest.fn().mockResolvedValue(uploadVersionEntityStub()),
     findOneBy: jest.fn().mockResolvedValue(uploadVersionEntityStub()),
     find: jest.fn().mockResolvedValue([uploadVersionEntityStub(), uploadVersionEntityStub()])
