3054aa739809d1fd93e55a9748df1fc9e3315dca
Ari Havlin <ayhavlin@gmail.com>
2023-07-12 12:50:19 +0300

    update orm db connection

--- common/src/database/config/ormconfig.ts
+++ common/src/database/config/ormconfig.ts
@@ -8,6 +8,8 @@ const ormConfig = new DataSource({
   host: process.env.POSTGRES_HOST,
   port: Number(process.env.POSTGRES_PORT),
   database: process.env.POSTGRES_DB,
+  username: process.env.POSTGRES_USER,
+
   ...getDBAuthParams(),
   entities: [
     UploadVersionEntity,
@@ -41,7 +43,6 @@ function getDBAuthParams(){
     }
     default: {
       return {
-        username: process.env.POSTGRES_USER,
         password: process.env.POSTGRES_PASSWORD,
       }
     }
