16cbab350c4f08daa529965f86c40309367a8944
Ari Havlin <ayhavlin@gmail.com>
2023-07-18 21:02:35 +0300

    Create Validator on the response of the Kafka

--- common/src/kafka/utils/kafka-sender.service.ts
+++ common/src/kafka/utils/kafka-sender.service.ts
@@ -1,11 +1,16 @@
+import { BadRequestException, Logger } from "@nestjs/common";
 import { Injectable } from "@nestjs/common/decorators";
 import { ConfigService } from "@nestjs/config";
 import { ClientKafka } from "@nestjs/microservices";
-import { Observable, timeout } from "rxjs";
+import { ClassConstructor, plainToInstance } from "class-transformer";
+import { validate } from "class-validator";
+import { Observable, timeout, map } from "rxjs";
 
 @Injectable()
 export class KafkaSenderService {
     private client: ClientKafka;
+    private readonly logger = new Logger(KafkaSenderService.name);
+
     constructor(private configService: ConfigService){}
 
 
@@ -24,8 +29,28 @@ export class KafkaSenderService {
         )
     }
 
+    sendAndValidate<T extends Object>(topic: string, data: any, ClassConstructor: ClassConstructor<T>, waitTime?: number): Observable<Promise<T>>{
+        waitTime = (waitTime) ? waitTime : parseInt(this.configService.get("MICROSERVICE_RESPONSE_WAIT_TIME"))
+        return this.client.send(
+            topic,
+            data
+        ).pipe(
+            timeout(waitTime),
+            map(async res => {
+                const validationObject = plainToInstance(ClassConstructor, res);
+                const errors = await validate(validationObject);
+                if (errors.length > 0) {
+                    this.logger.log(`Validation error for response of topic: ${topic}`);
+                    const constraints = errors.map((error) => Object.values(error.constraints ?? {})).flat();
+                    this.logger.debug(`error list: ${errors}`);
+                    throw new BadRequestException(constraints);
+                }
+                return res;
+            })
+        )
+    }
 
-    emit(topic: string, data: any): Observable<any>{
+     emit(topic: string, data: any): Observable<any>{
         return this.client.emit(
             topic,
             data
