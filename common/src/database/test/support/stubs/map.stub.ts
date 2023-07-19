ae0e9673b094420df06e17ec077743b59d08d764
Ari Havlin <ayhavlin@gmail.com>
2023-07-19 17:27:41 +0300

    Create map stubs and fix discovery test

---
+++ common/src/database/test/support/stubs/map.stub.ts
@@ -0,0 +1,15 @@
+import { MapEntity, MapImportStatusEnum } from "@app/common/database/entities";
+
+export const mapEntityStub = (): MapEntity => {
+  return {
+    catalogId: "map-catalog-id",
+    productId: "map-product-id",
+    productName: "map-product-name",
+    fileName: "file-name.ptg",
+    zoomLevel: 13,
+    createDate: new Date("2023-07-19T14:24:54.885Z"),
+    boundingBox: "35.71927028,32.01644704,35.72253451,32.01871780",
+    packageUrl: "url/to/map/package",
+    status: MapImportStatusEnum.START,
+  } as MapEntity
+}
