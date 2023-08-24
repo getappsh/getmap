ce8adad78badccb687651f3032cfa7d6740a548f
Ari Havlin <ayhavlin@gmail.com>
2023-08-24 13:25:53 +0300

    Move api-project-management dto to lib

---
+++ common/src/dto/project-management/index.ts
@@ -0,0 +1,12 @@
+export * from './dto/edit-project-member.dto'
+export * from './dto/project-config.dto'
+export * from './dto/project-member.dto'
+export * from './dto/project.dto'
+export * from './dto/remove-project-member.dto'
+
+export * from './stubs/edit-project-member.dto.stub'
+export * from './stubs/project-member.dto.stub'
+export * from './stubs/project.dto.sub'
+
+
+
