2c212ac5686d01bda84bf55ee40c93a41bd09d91
adminadmin1 <davidk@ravtech.co.il>
2023-08-27 14:31:57 +0300

    Added unit test for setProjectConfigOption

--- common/src/database/test/support/__mocks__/project-types.repo.mocks.ts
+++ common/src/database/test/support/__mocks__/project-types.repo.mocks.ts
@@ -3,12 +3,16 @@ import { categoryEntityStub, formationEntityStub, operationSystemEntityStub, pla
 export const mockCategoryRepo = () => {
   return {
     find: jest.fn().mockResolvedValue([categoryEntityStub(), categoryEntityStub()]),
+    findOne: jest.fn().mockImplementation(params => findOne(params, categoryEntityStub)),
+    save: jest.fn().mockImplementation(entity => Promise.resolve(entity)),
   }
 };
 
 export const mockFormationRepo = () => {
   return {
     find: jest.fn().mockResolvedValue([formationEntityStub(), formationEntityStub()]),
+    findOne: jest.fn().mockImplementation(params => findOne(params, formationEntityStub)),
+    save: jest.fn().mockImplementation(entity => Promise.resolve(entity)),
   }
 };
 
@@ -16,6 +20,8 @@ export const mockFormationRepo = () => {
 export const mockOperationSystemRepo = () => {
   return {
     find: jest.fn().mockResolvedValue([operationSystemEntityStub(), operationSystemEntityStub()]),
+    findOne: jest.fn().mockImplementation(params => findOne(params, operationSystemEntityStub)),
+    save: jest.fn().mockImplementation(entity => Promise.resolve(entity)),
   }
 };
 
@@ -23,6 +29,17 @@ export const mockOperationSystemRepo = () => {
 export const mockPlatformRepo = () => {
   return {
     find: jest.fn().mockResolvedValue([platformEntityStub(), platformEntityStub()]),
+    findOne: jest.fn().mockImplementation(params => findOne(params, platformEntityStub)),
+    save: jest.fn().mockImplementation(entity => Promise.resolve(entity)),
   }
 };
+
+const findOne = (params: any, entity: any) => {
+  if (params.where && params.where.name === entity().name) {
+    // Simulate behavior based on the input params
+    return Promise.resolve(entity());
+  } else {
+    return Promise.resolve(null); // No matching entity found
+  }
+}
 
