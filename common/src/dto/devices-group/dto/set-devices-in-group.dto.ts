ff0b9374d42f628c64e4c117c9b2eea08c955871
Ari Havlin <ayhavlin@gmail.com>
2023-11-05 17:19:41 +0200

    name fix

--- common/src/dto/devices-group/dto/set-devices-in-group.dto.ts
+++ common/src/dto/devices-group/dto/set-devices-in-group.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { IsArray, IsNumber, IsString } from "class-validator";
 
-export class SetDevicesInGroup {
+export class SetDevicesInGroupDto {
   @ApiProperty({required: true})
   @IsNumber()
   id: number
