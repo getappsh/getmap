fef2b870c5a11b400c9c73b386c53cb16ee8ead2
David Klein <davidk@ravtech.co.il>
2023-12-24 15:52:38 +0200

    Saved device-map-state when import create start, and updated the saving flaw when getting import status

--- common/src/dto/map/dto/create-import-res-dto.ts
+++ common/src/dto/map/dto/create-import-res-dto.ts
@@ -1,26 +1,12 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { MapImportStatusEnum } from "@app/common/database/entities";
-import { IsEnum, IsNotEmpty, IsString } from "class-validator";
 import { MapProductResDto } from "./map-product-res.dto";
-import { ErrorDto } from "../../error";
+import { ImportResDto } from "./import-res-dto";
 
-export class CreateImportResDto {
-
-  @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
-  importRequestId: string;
-
-  @ApiProperty({enum: MapImportStatusEnum})
-  @IsEnum(MapImportStatusEnum)
-  status: MapImportStatusEnum;
+export class CreateImportResDto extends ImportResDto {
 
   @ApiProperty({type: MapProductResDto})
   product: MapProductResDto
 
-  @ApiProperty({type: ErrorDto, required: false})
-  error: ErrorDto
- 
   toString() {
     return JSON.stringify(this);
   }
