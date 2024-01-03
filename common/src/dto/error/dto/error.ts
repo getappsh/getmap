6b9c90a080839aa1ab52d45eabd7a2a388496a98
David Klein <davidk@ravtech.co.il>
2024-01-03 13:15:01 +0200

    Updated offering res and started import create flow

--- common/src/dto/error/dto/error.ts
+++ common/src/dto/error/dto/error.ts
@@ -2,8 +2,8 @@ import { ApiProperty,  } from "@nestjs/swagger";
 import { IsEnum, IsString } from "class-validator";
 
 
-enum ErrorCode {
-  MAP_OTHER = 'MAP.other',
+export enum ErrorCode {
+  MAP_OTHER = 'MAP.unknown',
   MAP_REQUESTED_IN_PROCESSING = 'MAP.requestInProgress',
   MAP_AREA_TOO_LARGE = "MAP.areaTooLarge",
   MAP_AREA_TOO_SMALL = "MAP.areaTooSmall",
@@ -13,7 +13,7 @@ export class ErrorDto{
 
   @ApiProperty({
     enum: ErrorCode, description: 
-    "`MAP.other`: Error code not listed in the enum <br /> " +
+    "`MAP.unknown`: Error code not listed in the enum <br /> " +
     "`MAP.requestInProgress`: Delivery was already requested and in processing! <br /> " +
     "`MAP.areaTooLarge`: Area too large to distribute, reduce request size and try again <br /> " +
     "`MAP.areaTooSmall`: Area too small to distribute, increase request size and try again . "
