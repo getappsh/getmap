fd3b3bfdef12b1bd0f78e93c365b5e949c24bad7
David Klein <davidk@ravtech.co.il>
2024-01-16 16:42:39 +0200

    Handled set map configs

--- common/src/database/entities/map-config.entity.ts
+++ common/src/database/entities/map-config.entity.ts
@@ -1,31 +1,31 @@
 import { Column, Entity } from "typeorm";
 import { BaseEntity } from "./base.entity";
 
-@Entity()
+@Entity("map_configs")
 export class MapConfigEntity extends BaseEntity {
 
-  @Column({ name: "delivery_timeout" })
+  @Column({ name: "delivery_timeout", nullable: true })
   deliveryTimeout: number
  
-  @Column({ name: "max_map_size_meter", type: "bigint"})
+  @Column({ name: "max_map_size_meter", nullable: true})
   maxMapSizeInMeter: number
  
-  @Column({ name: "max_parallel_download" })
+  @Column({ name: "max_parallel_download", nullable: true })
   maxParallelDownloads: number
  
-  @Column({ name: "download_retry" })
+  @Column({ name: "download_retry", nullable: true })
   downloadRetryTime: number
  
-  @Column({ name: "download_timeout" })
+  @Column({ name: "download_timeout", nullable: true })
   downloadTimeoutSec: number
  
-  @Column({ name: "inventory_job_periodic_mins" })
+  @Column({ name: "inventory_job_periodic_mins", nullable: true })
   periodicForInventoryJob: number
   
-  @Column({ name: "map_conf_periodic_mins" })
+  @Column({ name: "map_conf_periodic_mins", nullable: true })
   periodicForMapConf: number
  
-  @Column({ name: "min_space_byte", type: "bigint" })
+  @Column({ name: "min_space_byte", nullable: true })
   minSpaceByte: number
 
   toString(){
