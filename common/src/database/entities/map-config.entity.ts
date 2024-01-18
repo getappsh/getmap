9ad5d5b799dd86cb0c76f17b27d3ec05c031a3ab
David Klein <davidk@ravtech.co.il>
2024-01-18 12:06:48 +0200

    Save get map changes of update map config table

--- common/src/database/entities/map-config.entity.ts
+++ common/src/database/entities/map-config.entity.ts
@@ -5,30 +5,36 @@ import { BaseEntity } from "./base.entity";
 export class MapConfigEntity extends BaseEntity {
 
   @Column({ name: "delivery_timeout", nullable: true })
-  deliveryTimeout: number
- 
-  @Column({ name: "max_map_size_meter", nullable: true})
+  deliveryTimeoutMins: number
+
+  @Column({ name: "max_map_size_meter", nullable: true })
   maxMapSizeInMeter: number
- 
+
+  @Column({ name: "max_map_size_MB", nullable: true })
+  maxMapSizeInMB: number
+
   @Column({ name: "max_parallel_download", nullable: true })
   maxParallelDownloads: number
- 
+
   @Column({ name: "download_retry", nullable: true })
   downloadRetryTime: number
- 
-  @Column({ name: "download_timeout", nullable: true })
-  downloadTimeoutSec: number
- 
-  @Column({ name: "inventory_job_periodic_mins", nullable: true })
-  periodicForInventoryJob: number
-  
-  @Column({ name: "map_conf_periodic_mins", nullable: true })
-  periodicForMapConf: number
- 
-  @Column({ name: "min_space_byte", nullable: true })
-  minSpaceByte: number
 
-  toString(){
+  @Column({ name: "download_timeout", nullable: true })
+  downloadTimeoutMins: number
+
+  @Column({ name: "inventory_job_periodic_mins", nullable: true })
+  periodicInventoryIntervalMins: number
+
+  @Column({ name: "map_conf_periodic_mins", nullable: true })
+  periodicConfIntervalMins: number
+
+  @Column({ name: "min_space_byte", nullable: true })
+  minAvailableSpaceBytes: number
+
+  @Column({ name: "matomo_url", nullable: true })
+  matomoUrl: string
+
+  toString() {
     return JSON.stringify(this)
   }
 }
