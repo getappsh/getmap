9705dcd0dff757b69fa37e34ab02e1472cda7da1
David Klein <davidk@ravtech.co.il>
2024-02-12 22:20:25 +0200

    Update endpoint of put device props

--- common/src/dto/device/dto/device-put.dto.ts
+++ common/src/dto/device/dto/device-put.dto.ts
@@ -1,13 +1,14 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsString } from "class-validator";
+import { IsOptional, IsString } from "class-validator";
 
 export class DevicePutDto {
 
   deviceId: string
 
   @ApiProperty({ required: false })
+  @IsOptional()
   @IsString()
-  deviceName: string
+  name: string
 
   toString() {
     return JSON.stringify(this);
