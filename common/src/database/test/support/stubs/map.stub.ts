415171197572b9af674acf206887357b94635be1
David Klein <davidk@ravtech.co.il>
2024-02-20 18:07:09 +0200

    Added test for repo service

--- common/src/database/test/support/stubs/map.stub.ts
+++ common/src/database/test/support/stubs/map.stub.ts
@@ -4,21 +4,20 @@ import { productEntityStubNorthGaza, productEntityStubNorthGazaRecent } from "./
 export const mapEntityStub = (): MapEntity => {
   return {
     catalogId: "map-catalog-id",
-    // TODO
-    // productId: "map-product-id",
-    // productName: "map-product-name",
     fileName: "file-name.ptg",
     zoomLevel: 13,
     createDateTime: new Date("2023-07-19T14:24:54.885Z"),
     boundingBox: "35.71927028,32.01644704,35.72253451,32.01871780",
-    packageUrl: "url/to/map/package",
+    packageUrl: null,
+    footprint: null,
     status: MapImportStatusEnum.START,
+    mapProduct: productEntityStubNorthGaza()
   } as MapEntity
 }
 
 export const updatedMapFromGazeNorthStub = (): MapEntity => {
   return {
-    catalogId: "map-catalog-id",
+    catalogId: "map-catalog-id-1",
     boundingBox: "34.50547135,31.43747849,34.53916147,31.45324041",
     footprint: "34.50547135,31.43747849,34.53916147,31.43747849,34.53916147,31.45324041,34.50547135,31.45324041,34.50547135,31.43747849",
     status: MapImportStatusEnum.DONE,
@@ -29,7 +28,7 @@ export const updatedMapFromGazeNorthStub = (): MapEntity => {
 
 export const obsoleteMapFromGazeNorthStub = (): MapEntity => {
   return {
-    catalogId: "map-catalog-id",
+    catalogId: "map-catalog-id-2",
     boundingBox: "34.50547135,31.43747849,34.53916147,31.45324041",
     footprint: "34.50547135,31.43747849,34.53916147,31.43747849,34.53916147,31.45324041,34.50547135,31.45324041,34.50547135,31.43747849",
     status: MapImportStatusEnum.DONE,
@@ -40,11 +39,40 @@ export const obsoleteMapFromGazeNorthStub = (): MapEntity => {
 
 export const obsoleteMapFromGazeNorthRecentStub = (): MapEntity => {
   return {
-    catalogId: "map-catalog-id",
+    catalogId: "map-catalog-id-3",
     boundingBox: "34.50547135,31.43747849,34.53916147,31.45324041",
     footprint: "34.50547135,31.43747849,34.53916147,31.43747849,34.53916147,31.45324041,34.50547135,31.45324041,34.50547135,31.43747849",
     status: MapImportStatusEnum.DONE,
     isUpdated: false,
     mapProduct: productEntityStubNorthGazaRecent()
+  } as MapEntity
+}
+
+
+export const expiredMap = (): MapEntity => {
+  return {
+    expiredDate: new Date(Date.now() - 10000),
+    status: MapImportStatusEnum.DONE
+  } as MapEntity
+}
+
+export const notExpiredMap = (): MapEntity => {
+  return {
+    expiredDate: new Date(Date.now() + 10000),
+    status: MapImportStatusEnum.DONE
+  } as MapEntity
+}
+
+export const withoutExpiredMap = (): MapEntity => {
+  return {
+    expiredDate: null,
+    status: MapImportStatusEnum.DONE
+  } as MapEntity
+}
+
+export const withoutExpiredMapInProgress = (): MapEntity => {
+  return {
+    expiredDate: null,
+    status: MapImportStatusEnum.IN_PROGRESS
   } as MapEntity
 }
