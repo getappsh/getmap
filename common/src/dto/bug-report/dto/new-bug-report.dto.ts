a2f8e4524fd27ec342a0bfb7373baf9b062150d1
Ari Havlin <aryehh@ravtech.co.il>
2024-06-05 15:34:17 +0300

    Bug report

---
+++ common/src/dto/bug-report/dto/new-bug-report.dto.ts
@@ -0,0 +1,23 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+
+export class NewBugReportDto {
+
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  deviceId: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  agentVersion: string
+  
+  @ApiProperty({required: false})
+  @IsString()
+  @IsOptional()
+  description: string
+
+}
