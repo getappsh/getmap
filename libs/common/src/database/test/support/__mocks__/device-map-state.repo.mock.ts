440831c764686c876f2c3514a232a2de629f00d0
Ari Havlin <ayhavlin@gmail.com>
2024-06-06 16:55:42 +0300

    Delete libs

--- libs/common/src/database/test/support/__mocks__/device-map-state.repo.mock.ts
+++
@@ -1,7 +0,0 @@
-import { deviceMapEntityStub } from "../stubs/device-map-state.stub";
-
-export const mockDeviceMapStateRepo = () => {
-  return {
-    findOneBy: jest.fn().mockResolvedValue(deviceMapEntityStub()),
-  }
-};
