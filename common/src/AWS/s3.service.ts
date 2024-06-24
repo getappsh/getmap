33c9132f119a823e58b7d7aa3b04c9942a7dff44
David Klein <davidk@ravtech.co.il>
2024-06-24 17:12:28 +0300

    Added checksum for downloading and uploading

--- common/src/AWS/s3.service.ts
+++ common/src/AWS/s3.service.ts
@@ -4,10 +4,11 @@ import { Progress, Upload } from "@aws-sdk/lib-storage";
 import { S3, PutObjectCommand, GetObjectCommand, PutObjectRequest, } from "@aws-sdk/client-s3";
 import { ConfigService } from '@nestjs/config';
 import { createReadStream, existsSync } from 'fs';
-import stream from 'stream';
+import stream, { Readable } from 'stream';
 import { Observable } from 'rxjs';
 import { HashDto } from '../dto/delivery/dto/delivery-item.dto';
 import { HashAlgorithmEnum } from '../database/entities/enums.entity';
+import { createHash } from 'crypto';
 
 @Injectable()
 export class S3Service {
@@ -114,10 +115,42 @@ export class S3Service {
       fileUpload.on("httpUploadProgress", (progress: Progress) => {
         observer.next(progress.loaded);
       });
+
       fileUpload.done()
         .then(() => observer.complete())
         .catch(err => observer.error(err));
     })
+  }
+
+  async getHashForFile(objectKey: string) {
+    this.logger.log(`Get head file ${objectKey} from s3 to calculate hash`);
+    const params = {
+      Bucket: this.bucketName,
+      Key: objectKey,
+    };
+    const hash = createHash('sha256');
+    try {
+      const { Body } = await this.s3.send(new GetObjectCommand(params))
+      if (!Body) {
+        throw new Error('Empty response body');
+      }
+
+      const readable = Readable.from(Body as Readable);
+
+      readable.on('data', (chunk) => {
+        hash.update(chunk);
+      });
+
+      await new Promise((resolve, reject) => {
+        readable.on('end', resolve);
+        readable.on('error', reject);
+      });
+
+      return hash;
+    } catch (err) {
+      this.logger.error(`Error get head file ${objectKey} from s3, ${err}`);
+      throw err
+    }
   }
 
   async deleteFile(objectKey: string) {
