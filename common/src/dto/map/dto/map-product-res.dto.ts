371354a0bffa66ada30a32ff139d340a367ea3dd
David Klein <davidk@ravtech.co.il>
2024-07-23 10:41:46 +0300

    Updated map-product-res.dto.ts

--- common/src/dto/map/dto/map-product-res.dto.ts
+++ common/src/dto/map/dto/map-product-res.dto.ts
@@ -89,11 +89,7 @@ export class MapProductResDto {
     product.productName = records["mc:productName"]
     product.productVersion = Number(records["mc:productVersion"])
     product.productType = records["mc:productType"]
-    console.log(records["mc:productSubType"]);
-    console.log(Number(records["mc:productSubType"]));
-    console.log(Number(records["mc:productSubType"]) || null);
-    
-    // product.productSubType = Number(records["mc:productSubType"]) || null ;
+    product.productSubType = Number(records["mc:productSubType"]) || null ;
     product.description = records["mc:description"];
     product.imagingTimeBeginUTC = new Date(records["mc:imagingTimeBeginUTC"]);
     product.imagingTimeEndUTC = new Date(records["mc:imagingTimeEndUTC"]);
