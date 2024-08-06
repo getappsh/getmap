684ca997dd92fdc7bfb4cb29e08c9878213314c4
Ari Havlin <ayhavlin@gmail.com>
2024-08-06 18:50:37 +0300

    Create new table for device config, migrate from map config

--- common/src/dto/device/dto/device-config.dto.ts
+++ common/src/dto/device/dto/device-config.dto.ts
@@ -1,10 +1,71 @@
+
+import { DeviceConfigEntity } from "@app/common/database/entities/device-config.entity";
 import { ApiProperty } from "@nestjs/swagger";
-import { IsEnum, IsNumber, IsOptional, IsString } from "class-validator";
-import { AndroidConfigDto, TargetStoragePolicy } from "../../device/dto/device-config.dto";
-/**
-  * @deprecated This field is deprecated and will be removed in the future.
-  */
-export class MapConfigDto {
+import { IsEnum, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
+
+
+export enum TargetStoragePolicy {
+  SD_ONLY = "SDOnly",
+  FLASH_THEN_SD = "FlashThenSD",
+  SD_THEN_FLASH = "SDThenFlash",
+  FLASH_ONLY = "FlashOnly"
+}
+
+export class BaseConfigDto{
+  @ApiProperty({ required: true })
+  @IsString()
+  @IsNotEmpty()
+  group: string
+
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  lastConfigUpdateDate: Date
+
+}
+
+
+export class WindowsConfigDto extends BaseConfigDto{
+
+  constructor(){
+    super();
+    this.group = 'windows'
+  }
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsNumber()
+  periodicInventoryIntervalMins: number
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsNumber()
+  periodicConfIntervalMins: number
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsNumber()
+  periodicMatomoIntervalMins: number
+
+
+  static fromConfigEntity(eConfig: DeviceConfigEntity): WindowsConfigDto{
+    let config = new WindowsConfigDto();
+    config.group = eConfig.group;
+    for (const key in eConfig.data) {
+      config[key] = eConfig.data[key]
+    }
+    return config
+  }
+
+
+}
+
+export class AndroidConfigDto extends BaseConfigDto{
+
+  constructor(){
+    super();
+    this.group = 'android'
+  }
 
   @ApiProperty({ required: false })
   @IsOptional()
@@ -121,35 +182,32 @@ export class MapConfigDto {
   @IsString()
   controlMapPath: string
 
-  static fromMapConfig(cE: AndroidConfigDto) {
-    const config = new MapConfigDto()
-    config.deliveryTimeoutMins = cE.deliveryTimeoutMins
-    config.downloadRetryTime = cE.downloadRetryTime
-    config.downloadTimeoutMins = cE.downloadTimeoutMins
-    config.MaxMapAreaSqKm = cE.MaxMapAreaSqKm
-    config.maxMapSizeInMB = cE.maxMapSizeInMB
-    config.maxParallelDownloads = cE.maxParallelDownloads
-    config.minAvailableSpaceMB = cE.minAvailableSpaceMB
-    config.periodicInventoryIntervalMins = cE.periodicInventoryIntervalMins
-    config.periodicConfIntervalMins = cE.periodicConfIntervalMins
-    config.periodicMatomoIntervalMins = cE.periodicMatomoIntervalMins
-    config.mapMinInclusionInPercentages = cE.mapMinInclusionInPercentages
-    config.matomoUrl = cE.matomoUrl
-    config.matomoDimensionId = cE.matomoDimensionId
-    config.matomoSiteId = cE.matomoSiteId
-    config.lastConfigUpdateDate = cE.lastConfigUpdateDate
-    config.sdStoragePath = cE.sdStoragePath
-    config.flashStoragePath = cE.flashStoragePath
-    config.targetStoragePolicy = cE.targetStoragePolicy
-    config.flashInventoryMaxSizeMB = cE.flashInventoryMaxSizeMB
-    config.sdInventoryMaxSizeMB = cE.sdInventoryMaxSizeMB
-    config.ortophotoMapPath = cE.ortophotoMapPath
-    config.controlMapPath = cE.controlMapPath
-
+  static fromConfigEntity(cE: DeviceConfigEntity) {
+    const config = new AndroidConfigDto()
+    config.group = cE.group;
+    for (const key in cE.data) {
+      config[key] = cE.data[key]
+    }
     return config
   }
 
   toString() {
     return JSON.stringify(this)
   }
+}
+
+
+
+export function fromConfigEntity(eConfig: DeviceConfigEntity): AndroidConfigDto | WindowsConfigDto{
+  let config: AndroidConfigDto | WindowsConfigDto;
+  if (eConfig.group === 'android'){
+      config = new AndroidConfigDto();
+  }else {
+    config = new WindowsConfigDto();
+  }  
+  config.lastConfigUpdateDate = eConfig.lastUpdatedDate;
+  for (const key in eConfig.data) {
+    config[key] = eConfig.data[key];
+  }
+  return config;
 }
