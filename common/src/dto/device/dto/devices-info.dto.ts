dc054e047f5dcd78d6ebc8bb048071452e653470
Ari Havlin <ayhavlin@gmail.com>
2024-10-10 16:46:27 +0300

    Support device analytics for map and software

--- common/src/dto/device/dto/devices-info.dto.ts
+++ common/src/dto/device/dto/devices-info.dto.ts
@@ -1,25 +1,47 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNumber } from "class-validator";
+import { Type } from "class-transformer";
+import { IsArray, IsNumber, IsString, ValidateNested } from "class-validator";
 
-export class DevicesStatisticInfo {
-
-  @ApiProperty()
-  @IsNumber()
-  count: number;
+export class DevicesStatisticUnit {
 
   @ApiProperty()
   @IsNumber()
-  updated: number
- 
+  sum: number;
+  
   @ApiProperty()
-  @IsNumber()
-  onUpdateProcess: number
- 
-  @ApiProperty()
-  @IsNumber()
-  updateError: number
+  @IsArray()
+  @IsString({ each: true })
+  devices: string[];
 
   toString() {
     return JSON.stringify(this);
   }
 }
+
+export class DevicesStatisticInfo {
+
+  @ApiProperty()
+  @ValidateNested()
+  @Type(() => DevicesStatisticUnit)
+  count: DevicesStatisticUnit;
+
+  @ApiProperty()
+  @ValidateNested()
+  @Type(() => DevicesStatisticUnit)
+  updated: DevicesStatisticUnit;
+ 
+  @ApiProperty()
+  @ValidateNested()
+  @Type(() => DevicesStatisticUnit)
+  onUpdateProcess: DevicesStatisticUnit;
+ 
+  @ApiProperty()
+  @ValidateNested()
+  @Type(() => DevicesStatisticUnit)
+  updateError: DevicesStatisticUnit;
+
+  toString() {
+    return JSON.stringify(this);
+  }
+}
+
