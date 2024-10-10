ac0f3de94290934660daaaba3b22fd9b3d2dae83
Ari Havlin <ayhavlin@gmail.com>
2024-10-10 17:17:12 +0300

    Change Payload to RpcPayload

--- apps/get-map/src/get-map.controller.ts
+++ apps/get-map/src/get-map.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Logger } from '@nestjs/common';
 import { GetMapService } from './get-map.service';
-import { EventPattern, MessagePattern, Payload } from '@nestjs/microservices';
+import { EventPattern, MessagePattern } from '@nestjs/microservices';
 import { GetMapTopics, GetMapTopicsEmit } from '@app/common/microservice-client/topics';
 import { CreateImportDto, CreateImportResDto, InventoryUpdatesReqDto } from '@app/common/dto/map';
 import { ImportResPayload } from '@app/common/dto/libot/dto/import-res-payload';
-import { MapConfigDto } from '@app/common/dto/map/dto/map-config.dto';
 import { MapPutDto } from '@app/common/dto/map/dto/map-put.dto';
+import { RpcPayload } from '@app/common/microservice-client';
 
 
 @Controller()
@@ -23,20 +23,20 @@ export class GetMapController {
   }
 
   @MessagePattern(GetMapTopics.CREATE_IMPORT)
-  async importCreate(@Payload() importDto: CreateImportDto): Promise<CreateImportResDto> {
+  async importCreate(@RpcPayload() importDto: CreateImportDto): Promise<CreateImportResDto> {
     this.logger.log("Start import create")
     return this.getMapService.importCreate(importDto)
   }
 
 
   @MessagePattern(GetMapTopics.GET_IMPORT_STATUS)
-  getImportStatus(@Payload('stringValue') reqId: string) {
+  getImportStatus(@RpcPayload('stringValue') reqId: string) {
     this.logger.log("Get import create status")
     return this.getMapService.getImportStatus(reqId)
   }
 
   @EventPattern(GetMapTopics.EXPORT_NOTIFICATION)
-  exportNotify(@Payload() payloadRaw: any) {
+  exportNotify(@RpcPayload() payloadRaw: any) {
     const payload = ImportResPayload.fromImportRes(payloadRaw)
     this.logger.log(`got notification from libot for job id ${payload.id} with status ${payload.status}`)
     return this.getMapService.handleNotification(payload)
@@ -44,7 +44,7 @@ export class GetMapController {
 
   // Inventory
   @MessagePattern(GetMapTopics.GET_INVENTORY_UPDATES)
-  getInventoryUpdates(@Payload() inventoryDto: InventoryUpdatesReqDto) {
+  getInventoryUpdates(@RpcPayload() inventoryDto: InventoryUpdatesReqDto) {
     this.logger.log("Get inventory updates")
     return this.getMapService.getInventoryUpdates(inventoryDto)
   }
@@ -56,7 +56,7 @@ export class GetMapController {
   }
 
   @MessagePattern(GetMapTopics.MAP_PUT)
-  putDeviceProperties(@Payload() p: MapPutDto): Promise<MapPutDto> {
+  putDeviceProperties(@RpcPayload() p: MapPutDto): Promise<MapPutDto> {
     return this.getMapService.putMapProperties(p)
   }
 
